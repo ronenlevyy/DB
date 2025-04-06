33756b422bc2c0ee51cf0e1d049d0fb78c70b82e
shahar.batito <shahar.batito@mail.huji.ac.il>
2025-04-06 11:41:54 +0300

    commit1

--- Ex1 Files-20250404/ex1.py
+++ Ex1 Files-20250404/ex1.py
@@ -1,42 +1,36 @@
-######
-# test
-####
-
-
 import csv
 from io import TextIOWrapper
 from zipfile import ZipFile
 
-# opens file.
+#create files
 enrollment_outfile = open("enrollment.csv", 'w' , encoding='UTF8')
 enrollment_outwriter = csv.writer(enrollment_outfile, delimiter=",", quoting=csv.QUOTE_MINIMAL)
 
-
-####################
-#todo - change names
-####################
-country_outfile = open("country.csv", 'w' , encoding='UTF8')
+country_outfile = open("Country.csv", 'w' , encoding='UTF8')
 country_outwriter = csv.writer(country_outfile, delimiter=",", quoting=csv.QUOTE_MINIMAL)
 
-incomegroup_outfile = open("incomegroup.csv", 'w' , encoding='UTF8')
+incomegroup_outfile = open("IncomeGroup.csv", 'w' , encoding='UTF8')
 incomegroup_outwriter = csv.writer(incomegroup_outfile, delimiter=",", quoting=csv.QUOTE_MINIMAL)
 
-region_outfile = open("region.csv", 'w' , encoding='UTF8')
+region_outfile = open("Region.csv", 'w' , encoding='UTF8')
 region_outwriter = csv.writer(region_outfile, delimiter=",", quoting=csv.QUOTE_MINIMAL)
 
-university_outfile = open("university.csv", 'w' , encoding='UTF8')
+university_outfile = open("University.csv", 'w' , encoding='UTF8')
 university_outwriter = csv.writer(university_outfile, delimiter=",", quoting=csv.QUOTE_MINIMAL)
 
-enrollemnt_year_outfile = open("enrollment_year.csv", 'w' , encoding='UTF8')
-enrollemnt_year_outwriter = csv.writer(enrollemnt_year_outfile, delimiter=",", quoting=csv.QUOTE_MINIMAL)
+closed_at_outfile = open("ClosedAt.csv", 'w' , encoding='UTF8')
+closed_at_outwriter = csv.writer(closed_at_outfile, delimiter=",", quoting=csv.QUOTE_MINIMAL)
 
-incomegroups = {}
+acceptance_rate_outfile = open("AcceptanceRate.csv", 'w' , encoding='UTF8')
+acceptance_rate_outwriter = csv.writer(acceptance_rate_outfile, delimiter=",", quoting=csv.QUOTE_MINIMAL)
+
+countries ={}
+income_groups = {}
 regions = {}
 universities = {}
-enrollment_years = {}
-########################
-########################
-########################
+closed_at = {}
+acceptance_rate = {}
+
 
 
 
@@ -46,16 +40,12 @@ def process_file():
       with zf.open('enrollment.csv', 'r') as infile:
           reader = csv.reader(TextIOWrapper(infile, 'utf-8'))
           for row in reader:
-              # TO DO splits row into the different csv table files
-              enrollment_outwriter.writerow(row)
-          ##################################
-          # todo make the right changes here
-          ##################################
+              process_row(row)
           for c in countries:
             country_outwriter.writerow(countries[c])
           country_outfile.close()
-          for in_group in incomegroups:
-                incomegroup_outwriter.writerow(incomegroups[in_group])
+          for in_group in income_groups:
+                incomegroup_outwriter.writerow(income_groups[in_group])
           incomegroup_outfile.close()
           for region in regions:
                 region_outwriter.writerow(regions[region])
@@ -63,26 +53,22 @@ def process_file():
           for u in universities:
                 university_outwriter.writerow(universities[u])
           university_outfile.close()
-          for y in enrollment_years:
-                enrollemnt_year_outwriter.writerow(enrollment_years[y])
-          enrollemnt_year_outfile.close()
-    ########################
-    ########################
-    ########################
-    enrollment_outfile.close()
+          for y in closed_at:
+                closed_at_outwriter.writerow(closed_at[y])
+          closed_at_outfile.close()
+          for y in acceptance_rate:
+              acceptance_rate_outwriter.writerow(acceptance_rate[y])
+          acceptance_rate_outfile.close()
 
 
 
-####################
-#todo - change names
-####################
+
 # process_row should splits row into the different csv table files
 def process_row(row):
-    #print(row)
     country = row[0]
     countrycode = row[1]
     region = row[2]
-    incomegroup = row[3]
+    income_group = row[3]
     iau_id1 = row[4]
     eng_name = row[5]
     orig_name = row[6]
@@ -96,27 +82,19 @@ def process_row(row):
     specialized = row[14]
     year = row[15]
     students5_estimated = row[16]
-    countries[countrycode]=[countrycode,country,incomegroup,region]
-    incomegroups[incomegroup]=[incomegroup]
+    countries[countrycode]=[countrycode,country,income_group,region]
+    income_groups[income_group]=[income_group]
     regions[region]=[region]
+    universities[iau_id1]=[iau_id1,eng_name,orig_name,foundedyr,yrclosed,private01,latitude,longitude,phd_granting,divisions,specialized,countrycode]
+    acceptance_rate[iau_id1,year]=[iau_id1,year,students5_estimated]
+    closed_at[iau_id1,year]=[iau_id1,year]
 
 
-universities[iau_id1]=[iau_id1,eng_name,orig_name,foundedyr,yrclosed,private01,latitude,longi tude,phd_granting,divisions,specialized,countrycode]
-enrollment_years[iau_id1,year]=[iau_id1,year,students5_estimated]
-########################
-########################
-########################
 
 # return the list of all tables
 def get_names():
-    return ["enrollment"]
-    ##################################
-    # todo make the right changes here
-    ##################################
-    # return ["country", "incomegroup", "region", "university", "enrollemnt_year"]
-    ########################
-    ########################
-    ########################
+    return ["Country", "Income_group", "Region", "University", "Closed_at", "Acceptance_rate"]
+
 
 
 if __name__ == "__main__":
